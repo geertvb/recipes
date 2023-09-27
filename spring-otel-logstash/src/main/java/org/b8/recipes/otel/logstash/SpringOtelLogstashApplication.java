6689cacd601a7bd9cce4fb7b4a4ec128a6173e73
Geert Van Bastelaere <geert.van.bastelaere@gmail.com>
2023-09-27 21:34:45 +0200

    Opentelemetry logstash logging (WIP)

---
+++ spring-otel-logstash/src/main/java/org/b8/recipes/otel/logstash/SpringOtelLogstashApplication.java
@@ -0,0 +1,13 @@
+package org.b8.recipes.otel.logstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOtelLogstashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOtelLogstashApplication.class, args);
+    }
+
+}
