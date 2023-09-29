f18baca48615244ee1aee99f09b34c84750a2dd1
Geert Van Bastelaere <geert.van.bastelaere@gmail.com>
2023-09-29 14:32:12 +0200

    Opentelemetry sdk logging

---
+++ spring-otel-sdk/src/main/java/org/b8/recipes/otel/logging/OtelLoggingApplication.java
@@ -0,0 +1,13 @@
+package org.b8.recipes.otel.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtelLoggingApplication.class, args);
+    }
+
+}
